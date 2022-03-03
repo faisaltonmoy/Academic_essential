9dae1a18faf204700b74773bb58b7fa54c1dd5dc
Faisal Ahmmed Tonmoy <42874834+faisaltonmoy@users.noreply.github.com>
2022-03-03 06:57:25 +0600

    on_1_2

---
+++ Compiler/Online/Task.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(void)
+{
+int a, b, c;
+float d, e, f, h, i;
+char x,y,z;
+scanf("%f%f%c",&h,&d,&x);
+printf("char %c float %f",x,d);
+scanf("%d%d%f%f",&b,&c,&e,&f);
+printf("float %f and %f",e,f);
+}
